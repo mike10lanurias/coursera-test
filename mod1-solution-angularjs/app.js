0bdfe4f98a922006f6bc373bcd4e6877fd84333f
Michael Lanurias <mlanuri10@gmail.com>
2024-12-06 20:17:54 -0500

    Completed Module 1 for Angular JS Course

---
+++ mod1-solution-angularjs/app.js
@@ -0,0 +1,32 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
+
+lunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope) {
+	$scope.lunchList = "";
+	$scope.message = "";
+
+	$scope.lunchMessage = function () {
+		var items = $scope.lunchList.split(",");
+		var numItems = 0;
+
+		for (var i = 0; i < items.length; i++) {
+			var item = items[i]?.trim(); // Removes whitespaces
+			if (item !== "") {
+				numItems++;
+			}
+		}
+
+		if (numItems <= 3) {
+			$scope.message = "Enjoy!";
+		}
+		else {
+			$scope.message =  "Too much!";
+		}
+	};
+};
+
+})();
