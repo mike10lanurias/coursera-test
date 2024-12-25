94c8f37d67d7d9732997a0f0bc2db886ecf9118c
Michael Lanurias <mlanuri10@gmail.com>
2024-12-25 15:15:29 -0500

    Completed Module 3

---
+++ module3-solution-angularjs/app.js
@@ -0,0 +1,76 @@
+(function() {
+    'use strict';
+
+    angular.module('NarrowItDownApp', [])
+        .controller('NarrowItDownController', NarrowItDownController)
+        .service('MenuSearchService', MenuSearchService)
+        .constant('ApiBasePath', "https://coursera-jhu-default-rtdb.firebaseio.com/")
+        .directive('foundItems', FoundItems);
+
+    function FoundItems() {
+        var ddo = {
+            restrict: 'E',
+            templateUrl: 'foundItems.html',
+            scope: {
+                foundItems: '<',
+                onEmpty: '<',
+                onRemove: '&'
+            },
+            controller: NarrowItDownController,
+            controllerAs: 'menu',
+            bindToController: true
+        };
+
+        return ddo;
+    }
+
+    NarrowItDownController.$inject = ['MenuSearchService'];
+
+    function NarrowItDownController(MenuSearchService) {
+        var menu = this;
+        menu.shortName = '';
+
+        menu.matchedMenuItems = function(searchTerm) {
+            var promise = MenuSearchService.getMatchedMenuItems(searchTerm);
+
+            promise.then(function(items) {
+                if (items && items.length > 0) {
+                    menu.message = '';
+                    menu.found = items;
+                } else {
+                    menu.message = 'Nothing found!';
+                    menu.found = [];
+                }
+            });
+        };
+
+        menu.removeMenuItem = function(itemIndex) {
+            menu.found.splice(itemIndex, 1);
+        }
+    }
+
+    MenuSearchService.$inject = ['$http', 'ApiBasePath'];
+
+    function MenuSearchService($http, ApiBasePath) {
+        var service = this;
+
+        service.getMatchedMenuItems = function(searchTerm) {
+            return $http({
+                method: "GET",
+                url: (ApiBasePath + "/menu_items.json")
+            }).then(function(response) {
+              var foundItems = [];
+              for (const key in response.data) {
+                for (var i = 0; i < response.data[key]['menu_items'].length; i++) {
+                  if(response.data[key]['menu_items'][i]['description'] &&
+                  response.data[key]['menu_items'][i]['description'].toLowerCase().includes(searchTerm.toLowerCase())) {
+                    foundItems.push(response.data[key]['menu_items'][i]);
+                  }
+                }
+              }
+
+              return foundItems;
+            });
+        };
+    }
+})();
